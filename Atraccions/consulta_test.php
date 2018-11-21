73c06fbac90c574a448260cf8d9a69186bee4ce0
Jose Manuel Febrer Cortés <jfebrerc@gmail.com>
2018-11-21 11:24:38 +0100

    Update Atraccions/consulta_test.php

--- Atraccions/consulta_test.php
+++ Atraccions/consulta_test.php
@@ -182,7 +182,61 @@ if (!isset($_SESSION['id_rol'])) :?>
           echo '      <td>'.$row["acces_express"].'</td>';
           echo '      <td>'.$row["data_creacio_registre"].'</td>';
           echo '      <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter'.$id_atraccio.'">
-                          Modificar
+          <div class="container">
+            <form>
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="example-text-input" placeholder="Email">
+              </div>
+            </div>
+            <div class="form-group row">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+            <div class="col-sm-10">
+            <input type="text" class="form-control" id="example-text-input" placeholder="Password">
+            </div>
+            </div>
+            <fieldset class="form-group row">
+            <legend class="col-form-legend col-sm-2">Radios</legend>
+            <div class="col-sm-10">
+            <div class="form-check">
+            <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+            Option one is this and that&mdash;be sure to include why its great
+            </label>
+            </div>
+            <div class="form-check">
+            <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+            Option two can be something else and selecting it will deselect option one
+            </label>
+            </div>
+            <div class="form-check disabled">
+            <label class="form-check-label">
+            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
+            Option three is disabled
+            </label>
+            </div>
+            </div>
+            </fieldset>
+            <div class="form-group row">
+            <label class="col-sm-2">Checkbox</label>
+            <div class="col-sm-10">
+            <div class="form-check">
+            <label class="form-check-label">
+            <input class="form-check-input" type="checkbox"> Check me out
+            </label>
+            </div>
+            </div>
+            </div>
+            <div class="form-group row">
+            <div class="offset-sm-2 col-sm-10">
+            <button type="submit" class="btn btn-primary">Sign in</button>
+            </div>
+            </div>
+            </form>
+            </div>
+
                       </button></td>';
           echo '      <td><a href=#> Eliminar </a></td>';
           echo '    </tr>';
