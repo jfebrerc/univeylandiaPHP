4cedb76cb7b063edcaa8f9127d69e5b801cebce6
jfebrerc <jfebrerc@gmail.com>
2018-11-21 16:18:07 +0100

    Update consulta_test.php

--- Atraccions/consulta_test.php
+++ Atraccions/consulta_test.php
@@ -260,18 +260,18 @@ if (!isset($_SESSION['id_rol'])) :?>
               </div>
             </div>
           </div>';
-            if (isset($_POST['modificar'])) {
-              $sql_update = "UPDATE ATRACCIO SET nom_atraccio='$nom_atraccio', tipus_atraccio='$tipus_atraccio', altura_min='$altura_min', altura_max='$altura_max', accessibilitat='$accessibilitat', acces_express='$acces_express' WHERE id_atraccio=$id_atraccio";
-                if (mysqli_query($conexio, $sql_update)) {
-                    echo "Record updated successfully";
-                } else {
-                    echo "Error updating record: " . mysqli_error($conexio);
-                }
-                //$modificar = modificar_atraccio($id_atraccio, $nom_atraccio, $tipus_atraccio, $altura_min, $altura_max, $accessibilitat, $acces_express);
-            }
         }
     } else {
         echo "0 results";
+    }
+    if (isset($_POST['modificar'])) {
+      $sql_update = "UPDATE ATRACCIO SET nom_atraccio='$nom_atraccio', tipus_atraccio='$tipus_atraccio', altura_min='$altura_min', altura_max='$altura_max', accessibilitat='$accessibilitat', acces_express='$acces_express' WHERE id_atraccio=$id_atraccio";
+        if (mysqli_query($conexio, $sql_update)) {
+            echo "Record updated successfully";
+        } else {
+            echo "Error updating record: " . mysqli_error($conexio);
+        }
+        //$modificar = modificar_atraccio($id_atraccio, $nom_atraccio, $tipus_atraccio, $altura_min, $altura_max, $accessibilitat, $acces_express);
     }
     echo '</table>';
     $conexio->close();
