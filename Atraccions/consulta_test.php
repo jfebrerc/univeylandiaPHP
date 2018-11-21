56275fdbfc8264aabbad0e508f37e1606a007c79
Jose Manuel Febrer Cortés <jfebrerc@gmail.com>
2018-11-21 11:37:22 +0100

    Update Atraccions/consulta_test.php

--- Atraccions/consulta_test.php
+++ Atraccions/consulta_test.php
@@ -201,28 +201,42 @@ if (!isset($_SESSION['id_rol'])) :?>
                   <p>'.$id_atraccio.'</p>
                   <div class="container">
                     <form>
+                    <div class="form-group row">
+                      <label for="example-text-input" class="col-2 col-form-label">Text</label>
+                      <div class="col-10">
+                        <input class="form-control" type="text" value="Artisanal kale" id="example-text-input">
+                      </div>
+                      </div>
                       <div class="form-group row">
-                        <label for="inputEmail3" class="col-sm-2 col-form-label">Nom atraccio</label>
-                        <div class="col-sm-10">
-                          <input type="text" class="form-control" id="example-text-input">
+                      <label for="example-search-input" class="col-2 col-form-label">Search</label>
+                      <div class="col-10">
+                        <input class="form-control" type="search" value="How do I shoot web" id="example-search-input">
                       </div>
                       </div>
-                        <div class="form-group row">
-                          <label for="inputPassword3" class="col-sm-2 col-form-label">Tipus atraccio</label>
-                            <div class="col-sm-10">
-                              <input type="text" class="form-control" id="example-text-input">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                          <label for="inputEmail3" class="col-sm-2 col-form-label">Altura min</label>
-                          <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input">
-                        </div>
-                        <div class="form-group row">
-                          <label for="inputEmail3" class="col-sm-2 col-form-label">Altura max</label>
-                          <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input">
-                        </div>
+                      <div class="form-group row">
+                      <label for="example-email-input" class="col-2 col-form-label">Email</label>
+                      <div class="col-10">
+                        <input class="form-control" type="email" value="bootstrap@example.com" id="example-email-input">
+                      </div>
+                      </div>
+                      <div class="form-group row">
+                      <label for="example-url-input" class="col-2 col-form-label">URL</label>
+                      <div class="col-10">
+                        <input class="form-control" type="url" value="https://getbootstrap.com" id="example-url-input">
+                      </div>
+                      </div>
+                      <div class="form-group row">
+                      <label for="example-tel-input" class="col-2 col-form-label">Telephone</label>
+                      <div class="col-10">
+                        <input class="form-control" type="tel" value="1-(555)-555-5555" id="example-tel-input">
+                      </div>
+                      </div>
+                      <div class="form-group row">
+                      <label for="example-password-input" class="col-2 col-form-label">Password</label>
+                      <div class="col-10">
+                        <input class="form-control" type="password" value="hunter2" id="example-password-input">
+                      </div>
+                      </div>
                       <div class="form-group row">
                         <div class="offset-sm-2 col-sm-10">
                           <button type="submit" class="btn btn-primary">Modificar</button>
