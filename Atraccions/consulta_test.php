4862971f62e6f613c819cb1d74695b9fc912cb18
Jose Manuel Febrer Cortés <jfebrerc@gmail.com>
2018-11-21 10:17:06 +0100

    Update Atraccions/consulta_test.php

--- Atraccions/consulta_test.php
+++ Atraccions/consulta_test.php
@@ -180,7 +180,9 @@ if (!isset($_SESSION['id_rol'])) :?>
           echo '      <td>'.$row["accessibilitat"].'</td>';
           echo '      <td>'.$row["acces_express"].'</td>';
           echo '      <td>'.$row["data_creacio_registre"].'</td>';
-          echo '      <td><a href=#> Modificar </a></td>';
+          echo '      <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                          Launch demo modal
+                      </button></td>';
           echo '      <td><a href=#> Eliminar </a></td>';
           echo '    </tr>';
           echo '  </tbody>';
@@ -193,7 +195,26 @@ if (!isset($_SESSION['id_rol'])) :?>
     $conexio->close();
 ?>
 
-
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 <div class="jumbotron text-center" width="100%" style="margin-bottom:0">
