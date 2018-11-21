fdb5ce2fd83975b00d2b190ba4ced6c42691145c
jfebrerc <jfebrerc@gmail.com>
2018-11-21 16:31:10 +0100

    Update consulta_test.php

--- Atraccions/consulta_test.php
+++ Atraccions/consulta_test.php
@@ -257,7 +257,7 @@ if (!isset($_SESSION['id_rol'])) :?>
                             $accessibilitat = $_POST['accessibilitatmod'];
                             $acces_express = $_POST['acces_expressmod'];
 
-                            $sql_update = "UPDATE ATRACCIO SET nom_atraccio='sdf', tipus_atraccio='$tipus_atraccio', altura_min='$altura_min', altura_max='$altura_max', accessibilitat='$accessibilitat', acces_express='$acces_express' WHERE id_atraccio=$id_atraccio";
+                            $sql_update = "UPDATE ATRACCIO SET nom_atraccio='$nom_atraccio', tipus_atraccio='$tipus_atraccio', altura_min='$altura_min', altura_max='$altura_max', accessibilitat='$accessibilitat', acces_express='$acces_express' WHERE id_atraccio=$id_atraccio";
                               if (mysqli_query($conexio, $sql_update)) {
                                   echo "Record updated successfully";
                               } else {
