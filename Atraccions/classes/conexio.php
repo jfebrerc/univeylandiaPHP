d23287d46fea9e708b90896c85acf975a6e9039d
Jose Manuel Febrer Cortés <jfebrerc@gmail.com>
2018-11-22 10:52:34 +0100

    s

---
+++ Atraccions/classes/conexio.php
@@ -0,0 +1,14 @@
+<?php
+    function crearConexio(){
+        $servidor   = "localhost";
+        $nombreBD   = "univeylandia";
+        $usuario    = "admin_web";
+        $contrasena = "Alumne123";
+        $conexio = new mysqli($servidor,$usuario,$contrasena,$nombreBD);
+        if($conexio->connect_error){
+            die("Error en la conexio : ".$conexion->connect_errno.
+                                      "-".$conexion->connect_error);
+        }
+        return $conexio;
+    }
+?>
