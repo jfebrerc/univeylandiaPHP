aae8f1af27b6a50718df99786077c66c5dde6063
manugallego <manuxp2010@gmail.com>
2018-11-28 17:21:53 +0100

    Update registrar_atraccio.php

--- Atraccions/registrar_atraccio.php
+++ Atraccions/registrar_atraccio.php
@@ -1,7 +1,7 @@
 
 <?php
 //include_once("php/class/class_atraccio.php");
-include_once ("classe_atraccio.php");
+include_once ("../classes/classe_atraccio.php");
 
 $atraccio = new Atraccio ($_POST['nom'],$_POST['tipusatraccio'],$_POST['datainauguracio'],$_POST['alturamin'],$_POST['alturamax'],$_POST['accessible'],$_POST['accesexpress']);
 
