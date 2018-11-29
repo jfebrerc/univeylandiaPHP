363e2a9501c614590acd05a1423982663d243379
jfebrerc <jfebrerc@gmail.com>
2018-11-29 17:16:16 +0100

    petadeta.php

---
+++ gestio/gestioAtraccio/registrar_atraccio.php
@@ -0,0 +1,19 @@
+
+<?php
+//include_once("php/class/class_atraccio.php");
+include_once $_SERVER['DOCUMENT_ROOT']."/php/classes/classeAtraccio.php";
+
+$Atraccio = new Atraccio ($_POST['nom'],$_POST['tipusatraccio'],$_POST['datainauguracio'],$_POST['alturamin'],$_POST['alturamax'],$_POST['accessible'],$_POST['accesexpress']);
+
+$Atraccio->Registrar();
+
+
+
+
+if($Atraccio->Registrar() == true){
+  //header('Location: ./registreAtraccions.php');
+    header('Location: ./registreAtraccions.php');
+}
+
+
+ ?>
