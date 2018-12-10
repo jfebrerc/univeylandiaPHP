3b292ea416a7993dd1a273ca3245937174da76a6
manugallego <manuxp2010@gmail.com>
2018-12-10 17:42:04 +0100

    comentat

--- JavaScript/captchaNumeral.js
+++ JavaScript/captchaNumeral.js
@@ -1,60 +1,100 @@
 
 
 
-
+var primerdigit;
+var segondigit;
+var operacio;
 function numerosRandom(){
-var primernumero = Math.floor(Math.random()* 10);
-var segonnumero = Math.floor(Math.random()* 10);
-var operacio =  Math.floor(Math.random()* 3);
-document.getElementById("primerdigit").innerHTML = primernumero;
-document.getElementById("simbol").innerHTML = operacio;
-document.getElementById("segondigit").innerHTML = segonnumero;
+ primerdigit = Math.floor(Math.random()* 10);
+ segondigit = Math.floor(Math.random()* 10);
+ operacio =  Math.floor(Math.random()* 3);
+var simbol ="";
+document.getElementById("primerdigit").innerHTML = primerdigit;
+if (operacio == 0){
+  simbol = "+";
+  document.getElementById("simbol").innerHTML = simbol;
 }
-function operacionesRandom(primernumero,segonnumero,operacio){
+else if (operacio == 1){
+  simbol = "-";
+  document.getElementById("simbol").innerHTML = simbol;
+}
+else{
+  simbol ="*";
+  document.getElementById("simbol").innerHTML = simbol;
+}
+
+document.getElementById("segondigit").innerHTML = segondigit;
+}
+
+
+
+function operacionesRandom(){
   var validar = document.getElementById("resposta").value;
-  var primerdigit = parseInt(document.getElementById("primerdigit").innerHTML);
-  var segondigit = parseInt(document.getElementById("segondigit").innerHTML);
+  var resultadobueno;
   if (operacio == 0){
-    eval("primerdigit + segondigit") ;
+    resultadobueno = eval(" primerdigit + segondigit");
     if (validar == null || validar == ""|| isNaN(validar)){
       alert("Introdueix un numero");
       return false;
     }
-    else if (validar != eval("primerdigit + segondigit")){
+    else if (validar != resultadobueno){
     alert ("Error has fallat");
     return false;
     }
-    else if (validar == eval("primerdigit + segondigit")){
-    alert("molt be fill de puta");
+    else if (validar == resultadobueno){
+    alert("molt be!");
     }
   }
 
   else if (operacio == 1) {
-     eval ("primerdigit - segondigit");
-     if (validar == null || validar == ""|| isNaN(validar)){
-       alert("Introdueix un numero");
-       return false;
-     }
-     else if (validar != eval("primerdigit - segondigit")){
-     alert ("Error has fallat");
-     return false;
-     }
-     else if (validar == eval("primerdigit - segondigit")){
-     alert("molt be fill de puta");
-     }
-  }
-  else {
-    eval(" primerdigit * segondigit");
+    resultadobueno = eval(" primerdigit - segondigit");
     if (validar == null || validar == ""|| isNaN(validar)){
       alert("Introdueix un numero");
       return false;
     }
-    else if (validar != eval("primerdigit * segondigit")){
+    else if (validar != resultadobueno){
     alert ("Error has fallat");
     return false;
     }
-    else if (validar == eval("primerdigit * segondigit")){
-    alert("molt be fill de puta");
+    else if (validar == resultadobueno){
+    alert("molt be!");
     }
   }
+  else {
+    resultadobueno=eval(" primerdigit * segondigit");
+    if (validar == null || validar == ""|| isNaN(validar)){
+      alert("Introdueix un numero");
+      return false;
+    }
+    else if (validar != resultadobueno){
+    alert ("Error has fallat");
+    return false;
+    }
+    else if (validar == resultadobueno){
+    alert("molt be!");
+    }
+  }
+}
+function finestraBannerAtraccio(){
+    //setTimeout(function(){ alert("Estas preparat per al Nadal?"); }, 5000);
+    myWindow1=window.open('','Ventana','width=720,height=480')
+	myWindow1.document.write('<html>')
+	myWindow1.document.write('<head>')
+	myWindow1.document.write('<title>UNIVEYLANDIA ATRACCIONS MES FAMOSES</title>')
+	myWindow1.document.write('</head>')
+	myWindow1.document.write('<body>')
+    myWindow1.document.write('<img src= "img/tornado.jpg" />')
+	myWindow1.document.write('</body>')
+	myWindow1.document.write('</html>')
+  myWindow1.setInterval(moveFinetraPromocio, 70);
+
+}
+
+function moveFinetraPromocio(){
+    myWindow1.moveBy(0, 1);
+    ++contador;
+    if (contador == 800){
+      myWindow1.close();
+    }
+
 }
--- gestio/gestioAtraccio/registrar_atraccio.php
+++ gestio/gestioAtraccio/registrar_atraccio.php
@@ -1,17 +1,22 @@
 
 <?php
-//include_once("php/class/class_atraccio.php");
 include_once("classes/classeAtraccio.php");
-
+/**
+ * [$Atraccio Creem un nou objecte de tipus Atraccio el qual mitjançant el metode _$POST li passarem el que hem escrit als formularis
+ * i es guardaran als atributs, seguidament cridarem a la funcio Registrar.]
+ *
+ */
 $Atraccio = new Atraccio ($_POST['nom'],$_POST['tipusatraccio'],$_POST['datainauguracio'],$_POST['alturamin'],$_POST['alturamax'],$_POST['accessible'],$_POST['accesexpress']);
-
+/**
+ * [$Atraccio->Registrar Una vegada creat el objecte fem servir la funció de registrar y li passem les dades. ]
+ * 
+ */
 $Atraccio->Registrar();
 
 
 
 
 if($Atraccio->Registrar() == true){
-  //header('Location: ./registreAtraccions.php');
     header('Location: ./registreAtraccions.php');
 }
 
