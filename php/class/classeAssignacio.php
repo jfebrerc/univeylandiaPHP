a0fbffed6c386ebdbf812dc1c1368c5fc84d9eb9
jfebrerc <jfebrerc@gmail.com>
2018-12-11 17:36:11 +0100

    Update classeAssignacio.php

--- php/class/classeAssignacio.php
+++ php/class/classeAssignacio.php
@@ -313,7 +313,8 @@ class Assignacio{
   //{
   //    die('Error de conexión: ' . $conexion->connect_error);
   //}
-  $busqueda = $_POST['buscar_assign'];
+  //$busqueda = $_POST['buscar_assign'];
+  $busqueda = "admin";
   //$_POST['busqueda_atraccio']
 
   $sql = "SELECT aua.id_assignacio, u.nom, u.cognom1, u.cognom2, u.numero_document, a.nom_atraccio, aua.data_inici_assign, aua.data_fi_assign, aua.data_creacio_registre FROM ASSIGN_USUARI_ATRACCIO aua LEFT JOIN ATRACCIO a ON aua.id_atraccio=a.id_atraccio LEFT JOIN USUARI u ON u.id_usuari=aua.id_usuari where u.nom like '%$busqueda%' or u.cognom1 like '%$busqueda%' or u.cognom2 like '%$busqueda%' or u.numero_document like '%$busqueda%' or a.nom_atraccio like '%$busqueda%' order by data_creacio_registre desc";
