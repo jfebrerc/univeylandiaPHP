bf23decce9bd6f2c2732a034f5a68802faf70123
manugallego <manuxp2010@gmail.com>
2018-12-12 20:40:35 +0100

    Update registreAssignacions.php

--- gestio/gestioAssignacioAE/registreAssignacions.php
+++ gestio/gestioAssignacioAE/registreAssignacions.php
@@ -334,16 +334,18 @@ if($_SESSION['rol'] != 3) {
         <div class="col-md-4">
           <?php
   		  include_once $_SERVER['DOCUMENT_ROOT']."/php/class/classeAtraccio.php";
-  		  echo '<form method="post" style="margin-top=50px;">
-  		  <div class="form-group row">
-  		  <div class="col-10">
-  			<input class="form-control" type="text" id="example-text-input" name="busqueda_atraccio" placeholder="Buscar...">
-  		  </div>
-  			<div class="form-group row">
-  			  <div class="offset-sm-2 col-sm-10">
-  				<input type="submit" class="btn btn-primary" name="buscar_atraccio" value="Buscar"">
-  		  </div>
-  			</div>
+  		  echo '
+        <form method="post" style="margin-top=50px;">
+  		    <div class="form-group row">
+  		        <div class="col-10">
+  			          <input class="form-control" type="text" id="example-text-input" name="busqueda_atraccio" placeholder="Buscar...">
+  		        </div>
+          </div>
+    			<div class="form-group row">
+    			  <div class="offset-sm-2 col-sm-10">
+    				<input type="submit" class="btn btn-primary" name="buscar_atraccio" value="Buscar"">
+    		  </div>
+  			 </div>
   		  </form>';
   		  //classeAtraccio = new Atraccio();
   		  if (isset($_POST['buscar_atraccio'])) {
