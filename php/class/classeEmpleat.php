3c73b3c34e74df2fb3114410c81c62918d16240e
manugallego <manuxp2010@gmail.com>
2018-12-12 13:47:17 +0100

    basura

--- php/class/classeEmpleat.php
+++ php/class/classeEmpleat.php
@@ -213,6 +213,214 @@ class Empleat {
     }
     $connection->close();
   }
+  public static function llistar_client(){
+    try{
+      $connection = crearConnexio();
+      $sql = "SELECT * FROM USUARI";
+      $resultat = $connection->query($sql);
+      echo '<table class="table table-bordered table-sm">';
+      echo '<thead>';
+      echo '<tr>';
+      echo '<th scope="col">Nom</th>';
+      echo '<th scope="col">Cognom1</th>';
+      echo '<th scope="col">Cognom2</th>';
+      echo '<th scope="col">Numero Document</th>';
+      echo '<th scope="col">Rol</th>';
+      echo '</tr>';
+      echo '</thead>';
+
+      if($resultat){
+        while($row = $resultat->fetch_assoc()){
+          $id = $row["id_usuari"];
+          $nom = $row["nom"];
+          $cognom1 = $row["cognom1"];
+          $cognom2 = $row["cognom2"];
+          $numero_document = $row["numero_document"];
+          $rol = $row["id_rol"];
+
+
+          echo '<tbody>';
+          echo '<tr>';
+          echo '<th scope="row">'.$row["nom"].'</th>';
+          echo '<td>'.$row["cognom1"].'</th>';
+          echo '<td>'.$row["cognom2"].'</th>';
+          echo '<td>'.$row["numero_document"].'</td>';
+          echo '<td>'.$row["id_rol"].'</td>';
+          echo '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalModificar'.$id.'"> Modificar</button></td>"';
+          echo '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ModalEliminar'.$id.'"> Eliminar<button></td>';
+          echo '</tr>';
+          echo '</tbody>';
+      echo '<!-- Modal -->
+      <div class="modal fade" id="modalModificar'.$id.'" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">Modificar zona</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="container">
+                <form method="post">
+                <div class="form-group row">
+                 <div class="col-10">
+                   <input class="form-control" type="text" value="'.$id.'" id="example-text-input" name="id" style="display: none;">
+                 </div>
+                </div>
+                </div>
+
+
+
+                <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">Nom</label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="text" value="'.$nom.'" id="example-text-input" name="nom_mod"">
+                  </div>
+                </div>
+                </div>
+
+
+              <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">Cognom1</label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="text" value="'.$cognom1.'" id="example-text-input" name="cognom1_mod"">
+                  </div>
+                </div>
+                </div>
+
+
+              <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">Cognom2</label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="text" value="'.$cognom2.'" id="example-text-input" name="cognom2_mod"">
+                  </div>
+                </div>
+                </div>
+
+
+                <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">Correu</label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="text" value="'.$email.'" id="example-text-input" name="email_mod"">
+                  </div>
+                </div>
+                </div>
+
+                <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">Password</label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="password" value="'.$contrasenya.'" id="example-text-input" name="contrasenya_mod"">
+                  </div>
+                </div>
+                </div>
+
+
+              <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">Data </label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="text" value="'.$date.'" id="example-text-input" name="date_mod"">
+                  </div>
+                </div>
+                </div>
+
+            <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">Adreça</label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="text" value="'.$adreca.'" id="example-text-input" name="adreca_mod"">
+                  </div>
+                </div>
+                </div>
+
+              <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">Ciutat</label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="text" value="'.$ciutat.'" id="example-text-input" name="ciutat_mod"">
+                  </div>
+                </div>
+                </div>
+
+              <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">Provincia</label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="text" value="'.$provincia.'" id="example-text-input" name="provincia_mod"">
+                  </div>
+                </div>
+                </div>
+
+
+              <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">CP</label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="text" value="'.$cp.'" id="example-text-input" name="cp_mod"">
+                  </div>
+                </div>
+                </div>
+
+              <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">Tipus document</label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="text" value="'.$tipus_document.'" id="example-text-input" name="tipus_document_mod"">
+                  </div>
+                </div>
+                </div>
+
+
+              <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">Numero document</label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="text" value="'.$numero_document.'" id="example-text-input" name="numero_document_mod"">
+                  </div>
+                </div>
+                </div>
+
+
+                <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">Sexe</label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="text" value="'.$sexe.'" id="example-text-input" name="sexe_mod"">
+                  </div>
+                </div>
+                </div>
+
+
+                <div class="form-group row">
+                  <label for="example-text-input" class="col-2 col-form-label">Telefon</label>
+                  <div class="form-group row">
+                  <div class="col-10">
+                    <input class="form-control" type="text" value="'.$telefon.'" id="example-text-input" name="telefon_mod"">
+                  </div>
+                </div>
+                </div>
+
+                  <div class="form-group row">
+                    <div class="offset-sm-2 col-sm-10">
+                      <input type="submit" class="btn btn-primary" name="modificar" value="Modificar"">';
+          echo'     </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tancar</button>
+          </div>
+          </div>
+        </div>
+      </div>';
 
 }
 
