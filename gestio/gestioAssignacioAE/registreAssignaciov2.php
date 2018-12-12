5ba9b892a076462bda7aef57ee6fede36a271df9
manugallego <manuxp2010@gmail.com>
2018-12-12 20:38:32 +0100

    dsa

--- gestio/gestioAssignacioAE/registreAssignaciov2.php
+++ gestio/gestioAssignacioAE/registreAssignaciov2.php
@@ -331,24 +331,77 @@ if($_SESSION['rol'] != 3) {
           <h1 class="h2">Registrar Assignacions Atraccio-Empleat</h1>
         </div>
 
-        <div class="col-md-4">
-          <?php
-  		  include_once $_SERVER['DOCUMENT_ROOT']."/php/class/classeAtraccio.php";
-  		  echo '<form method="post" style="margin-top=50px;">
-  		  <div class="form-group row">
-  		  <div class="col-10">
-  			<input class="form-control" type="text" id="example-text-input" name="busqueda_atraccio" placeholder="Buscar...">
-  		  </div>
-  			<div class="form-group row">
-  			  <div class="offset-sm-2 col-sm-10">
-  				<input type="submit" class="btn btn-primary" name="buscar_atraccio" value="Buscar"">
-  		  </div>
-  			</div>
-  		  </form>';
-  		  //classeAtraccio = new Atraccio();
-  		  if (isset($_POST['buscar_atraccio'])) {
-  			Atraccio::llistarEmpleatsBusqueda();
-        ?>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ModalAtraccions"> Sel·leccionar Atraccio
+        </button>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ModalEmpleats"> Sel·leccionar Empleats
+        </button>
+
+
+        <!-- Modal Atraccio-->
+        <div class="modal fade" id="ModalAtraccions" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered modal-lg " style="max-width: 80%" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Atenció!</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="container">
+                <form method="post">
+                  <?php
+                  include_once $_SERVER['DOCUMENT_ROOT']."/php/class/classeAtraccio.php";
+                  echo '<form method="post" style="margin-top=50px;">
+                       <div class="form-group row">
+                       <div class="col-10">
+                       <input class="form-control" type="text" id="example-text-input" name="busqueda_atraccio" placeholder="Buscar...">
+                       </div>
+                       <div class="form-group row">
+                         <div class="offset-sm-2 col-sm-10">
+                         <input type="submit" class="btn btn-primary" data-toggle="modal" data-target="#ModalAtraccions" name="buscar_atraccio" value="Buscar"">
+                       </div>
+                       </div>
+                       </form>';
+                       if (isset($_POST['buscar_atraccio'])) {
+                 		  Atraccio::SelecciollistarEmpleatsBusqueda();
+                 		  }
+                   ?>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <input type="submit" class="btn btn-primary" name="Acceptar" value="Acceptar">
+                </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="ModalEmpleats" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Atenció!</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="container">
+                <form method="post">
+                <input class="form-control" type="text" value="'.$id_atraccio.'" id="example-text-input" name="id_atraccioelim" style="display: none;">
+                Segur que vols eliminar la atracció: '.$nom_atraccio.'?
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <input type="submit" class="btn btn-primary" name="Acceptar" value="Acceptar">
+                </form>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
       </main>
