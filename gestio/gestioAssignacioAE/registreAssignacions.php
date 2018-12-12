790c30eec106987d4b8289cfc9d0d3126a5cbc28
manugallego <manuxp2010@gmail.com>
2018-12-12 17:58:10 +0100

    Update registreAssignacions.php

--- gestio/gestioAssignacioAE/registreAssignacions.php
+++ gestio/gestioAssignacioAE/registreAssignacions.php
@@ -339,7 +339,7 @@ if($_SESSION['rol'] != 3) {
 
         <!-- Modal Atraccio-->
         <div class="modal fade" id="ModalAtraccions" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+          <div class="modal-dialog modal-dialog-centered modal-lg " style="max-width: 80%" role="document">
             <div class="modal-content">
               <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLongTitle">Atenció!</h5>
