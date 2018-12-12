ab6031ee6121910011d61b76b1fd56871263068a
manugallego <manuxp2010@gmail.com>
2018-12-12 20:57:59 +0100

    Update registreAssignacions.php

--- gestio/gestioAssignacioAE/registreAssignacions.php
+++ gestio/gestioAssignacioAE/registreAssignacions.php
@@ -337,7 +337,7 @@ if($_SESSION['rol'] != 3) {
   		  echo '
         <form method="post" style="margin-top=50px;">
   		    <div class="form-group row">
-  		        <div class="col-10">
+  		        <div class="col-9">
   			          <input class="form-control" type="text" id="example-text-input" name="busqueda_atraccio" placeholder="Buscar...">
   		        </div>
               <input type="submit" class="btn btn-primary" name="buscar_atraccio" value="Buscar"">
