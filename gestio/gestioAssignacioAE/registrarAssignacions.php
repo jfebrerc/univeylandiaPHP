1d0ea7d5f9c00ee7e85fe4ca352306eb293f6dd4
manugallego <manuxp2010@gmail.com>
2018-12-13 21:12:37 +0100

    Update registrarAssignacions.php

--- gestio/gestioAssignacioAE/registrarAssignacions.php
+++ gestio/gestioAssignacioAE/registrarAssignacions.php
@@ -1,6 +1,8 @@
 <?php
 //include_once("php/class/class_atraccio.php");
 include_once $_SERVER['DOCUMENT_ROOT']."/php/class/classeAssignacio.php";
+include_once $_SERVER['DOCUMENT_ROOT']."/php/class/classeAtraccio.php";
+include_once $_SERVER['DOCUMENT_ROOT']."/php/class/classeEmpleat.php";
 
 $Atraccio = new Atraccio ($_POST['seleccio_empleat'],$_POST['seleccio_atraccio'],$_POST['datainauguracio'],$_POST['data_inici_assign'],$_POST['data_fi_assign']);
 
