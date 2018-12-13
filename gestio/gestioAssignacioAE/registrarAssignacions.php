83d651748e1a7b4971a4f97f2a809c627c637c86
manugallego <manuxp2010@gmail.com>
2018-12-13 21:19:43 +0100

    Update registrarAssignacions.php

--- gestio/gestioAssignacioAE/registrarAssignacions.php
+++ gestio/gestioAssignacioAE/registrarAssignacions.php
@@ -1,17 +1,15 @@
 <?php
 //include_once("php/class/class_atraccio.php");
 include_once $_SERVER['DOCUMENT_ROOT']."/php/class/classeAssignacio.php";
-include_once $_SERVER['DOCUMENT_ROOT']."/php/class/classeAtraccio.php";
-include_once $_SERVER['DOCUMENT_ROOT']."/php/class/classeEmpleat.php";
 
-$Atraccio = new Atraccio ($_POST['seleccio_empleat'],$_POST['seleccio_atraccio'],$_POST['datainauguracio'],$_POST['data_inici_assign'],$_POST['data_fi_assign']);
+$Assignacio = new Assignacio ($_POST['seleccio_empleat'],$_POST['seleccio_atraccio'],$_POST['datainauguracio'],$_POST['data_inici_assign'],$_POST['data_fi_assign']);
 
-$Atraccio->RegistrarAssignacio();
+$Assignacio->RegistrarAssignacio();
 
 
 
 
-if($Atraccio->RegistrarAssignacio() == true){
+if($Assignacio->RegistrarAssignacio() == true){
   //header('Location: ./registreAtraccions.php');
     header('Location: ./registreAtraccions.php');
 }
