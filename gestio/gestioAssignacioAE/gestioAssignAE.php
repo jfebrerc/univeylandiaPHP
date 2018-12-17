3f1eb121805f200c88da0dd151b2fcbe3adee344
Jose Manuel Febrer Cortés <jfebrerc@gmail.com>
2018-12-17 13:38:10 +0100

    Update gestioAssignAE.php

--- gestio/gestioAssignacioAE/gestioAssignAE.php
+++ gestio/gestioAssignacioAE/gestioAssignAE.php
@@ -328,7 +328,7 @@ if($_SESSION['rol'] != 3) {
 
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2">Registrar Assignacions Atraccio-Empleat</h1>
+          <h1 class="h2">Administrar assignacions atraccio-empleat</h1>
         </div>
         <?php
 		  include_once $_SERVER['DOCUMENT_ROOT']."/php/class/classeAssignacio.php";
