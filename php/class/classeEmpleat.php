4aa7dd9dff97dcc22baa83d883c96b66e94c8b82
manugallego <manuxp2010@gmail.com>
2018-12-17 21:02:17 +0100

    Update classeEmpleat.php

--- php/class/classeEmpleat.php
+++ php/class/classeEmpleat.php
@@ -284,7 +284,6 @@ class Empleat {
     echo '    </tr>';
     echo '  </thead>';
     if ($result) {
-        $arrayEmpleat = array(
       //echo ' <form method="post">';
         while($row = $result->fetch_assoc()) {
           $id_empleat = $row["id_usuari"];
@@ -328,7 +327,7 @@ class Empleat {
           echo '    </tr>';
           echo '  </tbody>';
         }
-      )
+    
       //  echo'</form>';
       }
 
