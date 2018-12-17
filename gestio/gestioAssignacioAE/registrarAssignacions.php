bd058b232130c051e43f678a939911c3ee5cb74d
manugallego <manuxp2010@gmail.com>
2018-12-17 16:07:08 +0100

    Update registrarAssignacions.php

--- gestio/gestioAssignacioAE/registrarAssignacions.php
+++ gestio/gestioAssignacioAE/registrarAssignacions.php
@@ -4,7 +4,7 @@ include_once $_SERVER['DOCUMENT_ROOT']."/php/class/classeAssignacio.php";
 //include_once $_SERVER['DOCUMENT_ROOT']."/php/class/classeAtraccio.php";
 //include_once $_SERVER['DOCUMENT_ROOT']."/php/class/classeEmpleat.php";
 
-foreach ($_POST['seleccio_empleat']){
+foreach ($_POST['seleccio_empleat'] as $array){
 $Assignacio = new Assignacio ($_POST['seleccio_empleat'],$_POST['seleccio_atraccio'],$_POST['data_inici_assign'],$_POST['data_fi_assign']);
 
 $comprovacio_registre = $Assignacio->RegistrarAssignacio();
