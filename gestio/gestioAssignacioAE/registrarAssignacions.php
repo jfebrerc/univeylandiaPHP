d2da0f7ece1f0334c03f902186f7ef279beb0590
manugallego <manuxp2010@gmail.com>
2018-12-18 16:59:09 +0100

    Update registrarAssignacions.php

--- gestio/gestioAssignacioAE/registrarAssignacions.php
+++ gestio/gestioAssignacioAE/registrarAssignacions.php
@@ -8,12 +8,12 @@ foreach ( $_POST['seleccio_empleat'] as $value){
   echo $value;
 //$Assignacio = new Assignacio ($value,$_POST['seleccio_atraccio'],$_POST['data_inici_assign'],$_POST['data_fi_assign']);
 
-$comprovacio_registre = $Assignacio->RegistrarAssignacio();
-if($comprovacio_registre == true){
+//$comprovacio_registre = $Assignacio->RegistrarAssignacio();
+//if($comprovacio_registre == true){
 
   //header('Location: ./registreAtraccions.php');
   //header('Location: ./registreAssignacions.php');
-}
+//}
 }
 
 
