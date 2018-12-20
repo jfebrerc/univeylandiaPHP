404a49d3ef9a745e430a3854919a1695706282c5
Jose Manuel Febrer Cortés <jfebrerc@gmail.com>
2018-12-20 14:11:58 +0100

    Update gestioFotos.php

--- gestio/gestioFotos/gestioFotos.php
+++ gestio/gestioFotos/gestioFotos.php
@@ -354,7 +354,7 @@ if($_SESSION['rol'] != 3) {
 
       <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                  <h1 class="h2">Administrar assignacions Empleats-Atraccions</h1>
+                  <h1 class="h2">Muntar fotos</h1>
                   <div class="btn-toolbar mb-2 mb-md-0">
                     <div class="btn-group mr-2">
                       <!--<form method="post" action="<?php //echo htmlentities($_SERVER['PHP_SELF']); ?>">-->
