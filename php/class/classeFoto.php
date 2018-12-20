b9cadb54a1aee0edd20dedabb34017d4aab92043
jfebrerc <jfebrerc@gmail.com>
2018-12-20 16:03:46 +0100

    Update classeFoto.php

--- php/class/classeFoto.php
+++ php/class/classeFoto.php
@@ -5,7 +5,7 @@ class Foto{
   public static function muntarFoto(){
     $target_dir = "../../fotos_atraccio/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $arxiu2 = $target_dir . "marcaAigua.".basename($_FILES["fileToUpload"]["name"]);
+    $arxiu2 = $target_dir . "marcaAigua_".basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
     if(isset($_POST["submit"])) {
