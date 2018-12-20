21a2aaeee4458074f766bf7e4253a8c7ee2ae21c
jfebrerc <jfebrerc@gmail.com>
2018-12-20 16:10:46 +0100

    Update classeFoto.php

--- php/class/classeFoto.php
+++ php/class/classeFoto.php
@@ -40,6 +40,21 @@ class Foto{
         }
         if (!copy($target_file, $arxiu2)) {
           echo "failed to copy";
+      }else {
+        $marca_aigua = "../../fotos_atraccio/marcaAigua.png";
+        $dest = imagecreatefrompng($arxiu2);
+        $src = imagecreatefromjpeg($marca_aigua);
+
+        imagealphablending($dest, false);
+        imagesavealpha($dest, true);
+
+        imagecopymerge($dest, $src, 10, 9, 0, 0, 181, 180, 100); //have to play with these numbers for it to work for you, etc.
+
+        header('Content-Type: image/png');
+        imagepng($dest);
+
+        imagedestroy($dest);
+        imagedestroy($src);
       }
     }
   }
