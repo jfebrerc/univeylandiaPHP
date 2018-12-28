dbce8cf68cd5983e3ade48288ac43a68c3498837
Jose Manuel Febrer Cortés <jfebrerc@gmail.com>
2018-12-28 14:42:05 +0100

    Update tenda.php

--- tenda.php
+++ tenda.php
@@ -451,36 +451,6 @@ if ($_SESSION['rol']==3 ) :?>
           body.appendChild(tbl);
       }
       //tableCreate();*/
-      var items = ['a','b','c'];
-      var properties = [1,2,3];
-      var selected = [];
-      var table= [[]];
-
-      for (var i = 0; i < items.length; i++) {
-      table.push([items[i],properties[i]]);
-      }
-
-      var renderTable = function() {
-      var data = document.getElementById('algo');
-
-      for (var i = 0; i < table.length; i++) {
-      var item = table[i][0];
-      var prop = table[i][1];
-
-      var row = document.createElement('tr');
-      var tdItem = document.createElement('td');
-      var tdProp = document.createElement('td');
-
-      tdItem.innerText = item;
-      tdProp.innerText = prop;
-
-      row.appendChild(tdItem);
-      row.appendChild(tdProp);
-
-      data.appendChild(row);
-      }
-      }
-      renderTable();
       document.write('<table id="data-table">');
       document.write('<tbody id="algo">');
       document.write('</tbody>');
