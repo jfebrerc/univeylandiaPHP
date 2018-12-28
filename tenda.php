5a06b994128942cf57b075fb613202a7b5d0e49c
Jose Manuel Febrer Cortés <jfebrerc@gmail.com>
2018-12-28 15:04:20 +0100

    Update tenda.php

--- tenda.php
+++ tenda.php
@@ -311,8 +311,9 @@ if ($_SESSION['rol']==3 ) :?>
 </nav>
 <?php endif ?>
 <div style="display: table; margin: 0 auto; margin-top: 50px;">
-  <script src="test.js"> </script>
   <table id="data-table"></table>
+  <script src="test.js"> </script>
+
 </div>
 
 <div class="jumbotron text-center" width="100%" style="margin-bottom:0">
