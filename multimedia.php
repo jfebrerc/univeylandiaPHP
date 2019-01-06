c34b988e477f4b3aab92c0c2681e4e149d5d72f3
Jose Febrer <jfebrerc@gmail.com>
2019-01-06 21:50:06 +0100

    Update multimedia.php

--- multimedia.php
+++ multimedia.php
@@ -311,7 +311,11 @@ if ($_SESSION['rol']==3 ) :?>
 </nav>
 <?php endif ?>
 <div style="display: table; margin: 0 auto; margin-top: 50px;">
-  <p> xd </p>
+  <video width="320" height="240" controls>
+  <source src="https://static.videezy.com/system/resources/previews/000/006/307/original/Rollercoaster.mp4" type="video/mp4">
+  <source src="movie.ogg" type="video/ogg">
+  Your browser does not support the video tag.
+  </video>
 </div>
 
 <div class="jumbotron text-center" width="100%" style="margin-bottom:0">
