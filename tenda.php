bedd0c0e9696f2284e3c0cb4cbc121fee890e526
jfebrerc <jfebrerc@gmail.com>
2019-01-16 17:35:43 +0100

    Update tenda.php

--- tenda.php
+++ tenda.php
@@ -315,6 +315,9 @@ if ($_SESSION['rol']==3 ) :?>
   <script src="test.js"> </script>
 
   <div id="taula_generada" style="margin-bottom: 50px;">
+    <div id="dino">
+      <h1 style="width: 500px; margin-left: 100px;"> Promoció: fes la compra ara i ahorra't els gastos d'enviament! </h1>
+    </div>
     <table id="data-table" class="table table-hover table-striped">
       <thead id="header_taula">
         <!--<tr>
@@ -325,9 +328,7 @@ if ($_SESSION['rol']==3 ) :?>
       <tbody id="tbody">
       </tbody>
     </table>
-    <div id="dino">
-      <h1 style="width: 500px; margin-left: 100px;"> Promoció: fes la compra ara i ahorra't els gastos d'enviament! </h1>
-    </div>
+
 </div>
 </div>
 
