9a3ded78e201265f218699a6f7153ef74fc85fa0
Jose Manuel Febrer Cortés <jfebrerc@gmail.com>
2019-02-06 13:54:08 +0100

    Update test.js

--- test.js
+++ test.js
@@ -240,12 +240,12 @@ $(document).ready(function(){
 
 function bigImg(x) {
   x.style.height = "64px";
-  x.style.width = "64px";
+  //x.style.width = "64px";
 }
 
 function normalImg(x) {
-  x.style.height = "32px";
-  x.style.width = "32px";
+  x.style.height = "38px";
+  //x.style.width = "32px";
 }
 
 $(document).ready(function(){
